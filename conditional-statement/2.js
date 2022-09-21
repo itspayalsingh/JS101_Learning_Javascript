d25685d9e1d315ef91a42a363bc414251420cee4
payal singh <ps56550856@gmail.com>
2022-09-22 01:53:16 +0530

    conditional statement

---
+++ conditional-statement/2.js
@@ -0,0 +1,3 @@
+chr="g";
+chr=="a"||chr=="e"||chr=="i"||chr=="o"||chr=="u"?console.log("Vowel")
+  :console.log("Consonant");
---
+++ conditional-statement/4.js
@@ -0,0 +1,11 @@
+
+//Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+let day = "sat";
+day == "sun"?console.log("sunday")
+  :day == "mon"?console.log("MONDAY")
+  :day == "tue"?console.log("Tuesday")
+  :day == "wed"?console.log("wednesday")
+  :day == "thu"?console.log("thursday")
+  :day == "fri"?console.log("friday")
+  :day == "sat"?console.log("saturday")
+  :console.log("NA");
